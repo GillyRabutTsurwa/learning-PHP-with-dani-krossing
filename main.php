3a086a3d3034cff5da9b99446dd14b01adabdc4d
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-01-27 12:05:17 -0600

    data types. not all, mais ceux les plus importants

--- main.php
+++ main.php
@@ -1,10 +1,33 @@
 <!-- variables in PHP. Très similaires à ceux en Javascript, à l'exception de "$" -->
 
 <?php
+// String
 $nom = "Gilbert";
-$age = 24;
+$ami = "Denisa";
+$language = "PHP";
 $nationality = "Kenyain";
 $saisonActuelle = "Hiver";
+
+// Integer
+$age = 24;
+$joursParSemaine = 7;
+$semainesParAn = 52;
+
+// Float
+$kmToMiles = 2.2;
+$pi = 3.1415;
+
+// Boolean 
+$isHandsome = true;
+$isFarFromGoal = true;
+$likesWinter = false;
+// true = 1 and false = 0;
+
+//Array
+$names = array("Gilbert", "Denisa", "Kim", "Diego", "Edin", "Teresa", "Ari");
+echo $names[0];
+
+
 
 // pour acceder la variable, il nous faut aussi la "$". fais attention.
 echo $nom;
