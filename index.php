e32b694889fd15ba578bb6373daa6f00d1d5aab3
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-01-27 11:29:18 -0600

    adding some more stuff here

--- index.php
+++ index.php
@@ -9,17 +9,19 @@
 
 <body>
     <!-- If we're doing PHP in conjunction with our HTML code, we do this. -->
+    <!-- on top of HTML, we can also put javascript in this code. it works fine. Thas coo -->
     <?php
     echo "Hello World from PHP";
+    // on peut utiliser le mot-clé print, mais à ce moment, je sais pas la diff.
+    print "Hello World from PHP (using print)"
     ?>
-    <p>PHP en HTML sans escaping</p>
+
+    <p>PHP dans notre HTML sans escaping</p>
     <?php
     if (1 === 1) echo "Ca marche m'ais n'est pas idéal";
     ?>
     <p>et avec escaping</p>
-    <?php {
-    ?> <p>C'est mieux, mais je n'aime toujours pas</p> <?php } ?>
-
+    <?php { ?> <p>C'est mieux, mais je n'aime toujours pas</p> <?php } ?>
     <!-- prettier n'aime pas PHP a ce moment -->
     <!-- j'espere au future on utilise le page pur PHP, sans HTML -->
     <a href="main.php">Pure PHP Page</a>
--- main.php
+++ main.php
@@ -1,5 +1,6 @@
 <!-- conversely, a document like this will only have PHP (a Pure PHP page) -->
 <!-- we do not include closing tag, ou bien, ca va rompre la site -->
 
+
 <?php
-    echo "Yoyoyo";
+echo "Yoyoyo"; // also, this is how you make a comment inside PHP code
