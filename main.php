1402a4054dec3ab82f57aeb6c293a45cbabfbbca
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-02-07 09:52:43 -0600

    come back and play around with more. plus put useful links. this is a very important branch

--- main.php
+++ main.php
@@ -1,40 +1,10 @@
-<!-- variables in PHP. Très similaires à ceux en Javascript, à l'exception de "$" -->
+On va apprendre en profondeur des fonctions plus tard
 
 <?php
-// String
-$nom = "Gilbert";
-$ami = "Denisa";
-$language = "PHP";
-$nationality = "Kenyain";
-$saisonActuelle = "Hiver";
+echo strlen("Bonjour Ça va?"); // like String.prototype.length in Javascript
+echo str_word_count("I have four words"); // don't know the equivalent in Javascript
+echo strrev("Function to reverse String");
+echo strpos("Where is Waldo located?", "Waldo");
+echo str_replace("Matthew", "John", "Matthew is the fourth Book");
 
-// Integer
-$age = 24;
-$joursParSemaine = 7;
-$semainesParAn = 52;
-
-// Float
-$kmToMiles = 2.2;
-$pi = 3.1415;
-
-// Boolean 
-$isHandsome = true;
-$isFarFromGoal = true;
-$likesWinter = false;
-// true = 1 and false = 0;
-
-//Array
-$names = array("Gilbert", "Denisa", "Kim", "Diego", "Edin", "Teresa", "Ari");
-echo $names[0];
-
-
-
-// pour acceder la variable, il nous faut aussi la "$". fais attention.
-echo $nom;
-echo $nationality;
-
-// constants are a bit different; plus similaire au javascript dans la sens qu'on utilise le mot-clé const. evidemment, on peut pas le rassigner.
-const birthDate = "9th May 1996";
-
-// how concatenating works in PHP
-echo $nom . " qu'il est beau";
+// there is a lot more. play around with these
