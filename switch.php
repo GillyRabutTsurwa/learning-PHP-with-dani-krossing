dedf404191e4bf5a9a63422a304d7086c5bce507
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-02-10 21:47:53 -0600

    switch statements. juss like javascript

---
+++ switch.php
@@ -0,0 +1,23 @@
+
+<?php
+// switch statements juss like in javascript, sert a faire plus lisible notre code si on a
+// beaucoup de conditions relatifs l'un vers l'autre
+$x = 4;
+
+switch ($x) {
+  case 1:
+    echo "The answer is 1";
+    break;
+  case 2:
+    echo "The answer is 1";
+    break;
+  case 3:
+    echo "The answer is 3";
+    break;
+  case 4:
+    echo "The answer is 4";
+    break;
+  default:
+    echo "J'ai marre de donner les reponses";
+    break;
+}
