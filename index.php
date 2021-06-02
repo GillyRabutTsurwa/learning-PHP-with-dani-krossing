ee1d44f374b82c9c4ee9abba2d8bcec1d5e93088
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2021-06-02 10:16:42 -0400

    git is hard. brought the branch back (technically i made a new one and pasted the code). we move

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
-</head>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
 
+        html {
+            font-size: 62.5%;
+        }
+
+        body {
+            font-size: 1.6rem;
+            font-family: Jost, sans-serif;
+            padding: 3rem;
+        }
+    </style>
+</head>
 <body>
     <!-- If we're doing PHP in conjunction with our HTML code, we do this. -->
-    <h2>Data Types</h2>
-    <h3>String, Integers, Floats & Booleans</h3>
-
-    <a href="main.php">Click to go to PHP Page</a>
+    <h2>Control Flow</h2>
+    <h3>Switch Statements</h3>
+    <a href="switch.php">Click to go to PHP Page</a>
 </body>
-
 </html>
---
+++ switch.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * NOTE:
+ * switch statements juss like in javascript, sert a faire plus lisible notre code si on a
+ * beaucoup de conditions relatifs l'un vers l'autre
+ */
+
+echo "<h4>Changer la valeur de x (qui est un chiffre) pour voir les different resultats</h4>";
+$x = 4;
+
+switch ($x) {
+  case 1:
+    echo "The answer is 1";
+    break;
+  case 2:
+    echo "The answer is 2";
+    break;
+  case 3:
+    echo "The answer is 3";
+    break;
+  case 4:
+    echo "The answer is 4";
+    break;
+  case 5:
+    echo "The answer is 5";
+    break;
+  case 6:
+    echo "The answer is 6";
+    break;
+  case 7:
+    echo "The answer is 7";
+    break;
+  default:
+    echo "J'ai marre de donner les reponses";
+    break;
+}
